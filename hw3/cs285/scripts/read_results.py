d0e2323fe5f6725fbbb4095e902dc85fdc50f6c1
alescontrela <alejandroescontrela@gmail.com>
2021-11-02 14:41:08 -0700

    Finish HW3 report

--- hw3/cs285/scripts/read_results.py
+++ hw3/cs285/scripts/read_results.py
@@ -1,26 +1,51 @@
 import glob
 import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
 
 def get_section_results(file):
     """
         requires tensorflow==1.12.0
     """
-    X = []
     Y = []
-    for e in tf.train.summary_iterator(file):
+    for e in tf.compat.v1.train.summary_iterator(file):
         for v in e.summary.value:
-            if v.tag == 'Train_EnvstepsSoFar':
-                X.append(v.simple_value)
-            elif v.tag == 'Eval_AverageReturn':
+
+            if v.tag == 'Train_AverageReturn':
                 Y.append(v.simple_value)
-    return X, Y
+    return Y
 
 if __name__ == '__main__':
     import glob
 
-    logdir = 'data/q1_lb_rtg_na_CartPole-v0_27-09-2021_01-03-36/events*'
-    eventfile = glob.glob(logdir)[0]
+    dqn_logdirs = [
+        'data/q2_dqn_1_LunarLander-v3_19-10-2021_20-18-27/events*',
+        'data/q2_dqn_2_LunarLander-v3_19-10-2021_20-25-14/events*',
+        'data/q2_dqn_3_LunarLander-v3_19-10-2021_20-26-26/events*'
+    ]
 
-    X, Y = get_section_results(eventfile)
-    for i, (x, y) in enumerate(zip(X, Y)):
-        print('Iteration {:d} | Train steps: {:d} | Return: {}'.format(i, int(x), y))
+    doubledqn_logdirs = [
+        'data/q2_doubledqn_1_LunarLander-v3_19-10-2021_20-24-17/events*',
+        'data/q2_doubledqn_2_LunarLander-v3_19-10-2021_20-25-41/events*',
+        'data/q2_doubledqn_3_LunarLander-v3_19-10-2021_20-26-46/events*'
+    ]
+
+    dqn_eventfiles = [
+        glob.glob(logdir)[0] for logdir in dqn_logdirs]
+    doubledqn_eventfiles = [
+        glob.glob(logdir)[0] for logdir in doubledqn_logdirs]
+
+    dqn_avg_returns = np.stack([
+        get_section_results(eventfile) for eventfile in dqn_eventfiles])
+    doubledqn_avg_returns = np.stack([
+        get_section_results(eventfile) for eventfile in doubledqn_eventfiles])
+
+    dqn_avg_returns = np.mean(dqn_avg_returns, axis=0)
+    doubledqn_avg_returns = np.mean(doubledqn_avg_returns, axis=0)
+
+    plt.plot(dqn_avg_returns, label='dqn return')
+    plt.plot(doubledqn_avg_returns, label='doubledqn avg return')
+    plt.legend()
+    plt.ylim([-100, 150])
+    plt.show()
+
---
+++ hw3/submit/cs285/policies/argmax_policy.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+class ArgMaxPolicy(object):
+
+    def __init__(self, critic):
+        self.critic = critic
+
+    def get_action(self, obs):
+        if len(obs.shape) > 3:
+            observation = obs
+        else:
+            observation = obs[None]
+        
+        action = self.critic.qa_values(obs)
+        action = np.argmax(action)
+        ## TODO return the action that maxinmizes the Q-value 
+        # at the current observation as the output
+        return action.squeeze()
